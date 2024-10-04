1113419ac398e0e997f59e772fb61e146b43227b
Inayah Ramadhani <naiversal@gmail.com>
2024-10-04 15:15:35 +0800

    livecode pertemuan 5

---
+++ Kelas B1 2024/Pertemuan 5/live code pertemuan 5.py
@@ -0,0 +1,84 @@
+# data = [1, 2, 3, 4, True, [2, 3], "Adi"]
+# data_2 = list((1, 2, 3, 4, 5))
+
+# print(data[6])
+# print(type(data_2))
+
+# data = ["apel", "durian", "jeruk"]
+
+# print(len(data))
+# print(data)
+# print()
+# data.extend["rambutan", "semangka"]
+# data.append("semangka")
+# data.append(True)
+# print(data)
+# data.insert(0, "rambutan")
+# print(data)
+
+# print(data_2)
+
+# data = ["apel", "durian", "jeruk"]
+# print(data)
+# print()
+# del data[0]
+# print(data)
+
+# data = ["apel", "durian", "jeruk"]
+# print(data*2)
+# print()
+# data.remove("apel")
+# print(data)
+# data.pop(1)
+# print(data)
+
+# data_mhs = [
+#     ["060", "ifnu",["APD", "ORSIKOM"]],
+#     ["065", "adi"]
+# ]
+
+# print(data_mhs[0][2][0])
+
+# data_mhs = [
+#     ["admin", "admin123", "admin"],
+#     ["user", "user123", "user"]
+# ]
+
+# print(data_mhs[0][2][0])
+
+# buah = ["apel", "durian", "jeruk"]
+
+# for data in buah:
+#     print(data)
+
+# buah = ["apel", "durian", "jeruk"]
+
+# for data in buah:
+#     print(f"data ke {index}")
+#     print(data)
+#     print 
+
+# buah = ["apel", "durian", "jeruk"]
+
+# for index in range (len(buah)):
+#     print(f"data ke {index+1}")
+#     print(buah(index))
+#     print("="*10)
+
+# buah = ["apel", "durian", "jeruk"]
+
+# for index, item in enumerate(buah, 2):
+#     print(f"data ke {index + 1}")
+#     print(item)
+#     print("="*10)
+
+# buah = ["apel", "durian", "jeruk"]
+# (buah1 * banyak_buah * buah):
+#     print(buah1)
+#     print(banyak_buah)
+# buah1, buah2, buah3 = buah
+# print(buah1)
+# print(buah2)
+# print(buah3)
+
+# maaf bang ga lengkap, sakit perut saya, asam lambung saya lagi kumat ):
